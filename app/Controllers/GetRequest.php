59404993d41deac258fe046e9a5596da52257374
Djonasus <60808389+Djonasus@users.noreply.github.com>
2022-08-07 20:40:36 +0300

    Update GetRequest.php

--- app/Controllers/GetRequest.php
+++ app/Controllers/GetRequest.php
@@ -5,34 +5,41 @@ namespace App\Controllers;
 class GetRequest extends \MainController
 {
 
-    public function index() {
+      public function index() {
 
-        //На всякий случай, я создал базу запросов, с возможностью её дополнять. В будущем, можно её вывести в какой-нибудь конфиг-файл.
-
-        $functionBase = array(
-            "testMe" => function($id, $par) { //тестовая функция для проверки
-              return ('{"jsonrpc":"2.0","result": "'.$par.'","id": "'.$id.'"}');
-
-            },"getTime" => function($id,$par) { //наша функция
-              if ($par == "0") { //если нам нужно текущее время
-                  $result = date('Y-m-d H:i:s', time());
-                  return ('{"jsonrpc":"2.0","result": "'.$result.'","id": "'.$id.'"}');
-              } else if($par == "1") { //это для времени Unix
-                  return ('{"jsonrpc":"2.0","result": "'.microtime().'","id": "'.$id.'"}');
-              }
-            }
-            
-        );
+        function errorMsg() {
+          die ('{"jsonrpc":"2.0","error": {"code": -32600, "message": "Invalid Request"},"id": null}'); //Инкапсулируем функцию ошибки
+        }
 
         if (isset($_POST) && $_SERVER["CONTENT_TYPE"] == 'application/json') { //Банальная проверка шапки запроса
-            $req = json_decode(file_get_contents('php://input'), true);
-            if (isset($functionBase[$req['method']])) {
-                return $functionBase[$req['method']]($req['id'],$req['params']);
-            } else{
-                return ('{"jsonrpc":"2.0","error": {"code": -32600, "message": "Invalid Request"},"id": null}');
+          $req = json_decode(file_get_contents('php://input'), true);
+          $method = $req["method"];
+          $par = $req["params"];
+
+          //Собираем наш ответ.
+
+          $json = array(
+            "jsonrpc" => "2.0",
+            "id" => $req["id"]
+          );
+
+          if ($method == "testMe") { //тестовая функция
+            $json["result"] = $par;
+          } else if ($method == "getTime") { //наша функция на вывод времени
+            if ($par == "0") { //текущее время
+              $json["result"] = date('Y-m-d H:i:s', time());
+            } else if ($par == "1") { //Unix
+              $json["result"] = microtime();
             }
+          } else {
+            errorMsg();
+          }
+
+          $sjson = json_encode($json);
+          return $sjson;
+
         } else {
-            return ('{"jsonrpc":"2.0","error": {"code": -32600, "message": "Invalid Request"},"id": null}');
+          errorMsg();
         }
     }
 }
